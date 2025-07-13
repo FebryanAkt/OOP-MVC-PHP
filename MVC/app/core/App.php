298e611ba466cdfb0dac9e69a601ae4127932df4
FebryanAkt <febryan08022006@gmail.com>
2025-07-13 19:38:37 +0700

    routing

--- MVC/app/core/App.php
+++ MVC/app/core/App.php
@@ -1,6 +1,15 @@
 <?php
     class App {
         public function __construct(){
-            echo "ok";
+            $url = $this->parseURL();
+            var_dump($url);
+        }
+        public function parseURL(){
+            if (isset ($_GET["url"])) {
+                $url = rtrim($_GET["url"], "/"); //rtrim untuk menghilangkan / pada akhir url
+                $url = filter_var($url, FILTER_SANITIZE_URL); //agar url bersih dari karakter aneh
+                $url = explode("/", $url); //untuk memecah  string $url menjadi array
+                return $url;
+            }
         }
     }
