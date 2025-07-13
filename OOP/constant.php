a6c2557ce522fd5ae27033ac339ef4696e19fbdf
FebryanAkt <febryan08022006@gmail.com>
2025-07-13 14:42:34 +0700

    constant

---
+++ OOP/constant.php
@@ -0,0 +1,14 @@
+<?php
+    define("NAMA", "Febryan Akhmad");
+    // echo NAMA;
+
+    echo "<br>";
+
+    const UMUR = "19";
+    // echo UMUR;
+
+    class Coba { 
+        const HALO = "halo"; //kalau menggunakan class harus memakai const
+    }
+    // echo Coba::HALO;
+?>
