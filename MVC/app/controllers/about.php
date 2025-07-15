408f05c3820e37151141142d2dd13bb1199841a1
FebryanAkt <febryan08022006@gmail.com>
2025-07-15 17:19:51 +0700

    controller

---
+++ MVC/app/controllers/about.php
@@ -0,0 +1,10 @@
+<?php
+    class About {
+        // method default
+        public function index($nama = "febryan", $status = "ban dalam"){
+            echo "Halo, saya $nama, saya seorang $status";
+        }
+        public function page(){
+            echo "About/page";
+        }
+    }    
