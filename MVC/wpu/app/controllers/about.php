af8964e64dee6e912941048df4f10924a6e398fb
FebryanAkt <febryan08022006@gmail.com>
2025-07-19 19:15:47 +0700

    view

--- MVC/wpu/app/controllers/about.php
+++ MVC/wpu/app/controllers/about.php
@@ -1,10 +1,19 @@
 <?php
-    class About {
-        // method default
-        public function index($nama = "febryan", $status = "ban dalam"){
-            echo "Halo, saya $nama, saya seorang $status";
+    class About extends Controller{
+        public function index($nama = "febryan", $pekerjaan = "mahasiswa", $umur = 19){
+            $data["nama"] = $nama;
+            $data["pekerjaan"] = $pekerjaan;
+            $data["umur"] = $umur;
+            $data["judul"] = "About Me";
+            $this->view("templates/header", $data);
+            $this->view('about/index', $data);
+            $this->view("templates/footer");
         }
+
         public function page(){
-            echo "About/page";
+            $data["judul"] = "Pages";
+            $this->view("templates/header", $data);            
+            $this->view('about/page');
+            $this->view("templates/footer");
         }
     }    
